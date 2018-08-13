9506fed19db2d7ccc458ab97bb5d310f5a0e4072
Benjamin Misell <benjamin@misell.cymru>
2018-08-13 12:14:12 +0100

    Finish iPhone slider

--- wwfypc-react/deploy.sh
+++ wwfypc-react/deploy.sh
@@ -5,4 +5,4 @@ HASH=`git log --pretty=format:'%H' -n 1`
 docker build -t evilben/wwfypc-react:$HASH .
 docker push evilben/wwfypc-react:$HASH
 cat kubes/deploy.yaml | sed "s/(hash)/$HASH/g" | kubectl apply -f -
-kubectl -n lmi rollout status deployment/wwfypc-react
+kubectl -n wwfypc rollout status deployment/wwfypc-react
