b3133921553e14ad46ee36a698443c75df9df327
Benjamin Misell <benjamin@misell.cymru>
2018-08-18 08:49:57 +0100

    nginx docker

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,12 @@ HASH=`git log --pretty=format:'%H' -n 1`
 
 docker build -t evilben/wwfypc-django:$HASH wwfypc-django
 docker build -t evilben/wwfypc-react:$HASH wwfypc-react
+docker build -t evilben/wwfypc-nginx:$HASH config/nginx
 docker push evilben/wwfypc-react:$HASH
-cat kubes/deploy.yaml | sed "s/(hash)/$HASH/g" | kubectl apply -f -
-kubectl -n wwfypc rollout status deployment/wwfypc-react
+docker push evilben/wwfypc-django:$HASH
+docker push evilben/wwfypc-nginx:$HASH
+cat wwfypc-react/kubes/deploy.yaml | sed "s/(hash)/$HASH/g" | kubectl apply -f -
+cat kubes/gluster.yaml | kubectl apply -f -
+cat kubes/mysql.yaml | kubectl apply -f -
+cat kubes/django.yaml | sed "s/(hash)/$HASH/g" | kubectl apply -f -
+cat kubes/nginx.yaml | sed "s/(hash)/$HASH/g" | kubectl apply -f -
--- wwfypc-django/start.sh
+++ wwfypc-django/start.sh
@@ -3,6 +3,7 @@
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
 echo Starting Gunicorn.
-exec gunicorn lmisite.wsgi:application \
+export DJANGO_SETTINGS_MODULE=wwfypc.settings_test
+exec gunicorn wwfypc.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 5
