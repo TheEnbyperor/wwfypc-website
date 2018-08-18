2c81bff63114dc62056774d6882396bfeaf7ce0a
Benjamin Misell <benjamin@misell.cymru>
2018-08-18 12:19:20 +0100

    correct settings for django migrations

--- wwfypc-django/start.sh
+++ wwfypc-django/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 python3 manage.py collectstatic --no-input
-python3 manage.py migrate
+python3 manage.py migrate --settings wwfypc.settings_test
 echo Starting Gunicorn.
 export DJANGO_SETTINGS_MODULE=wwfypc.settings_test
 exec gunicorn wwfypc.wsgi:application \
