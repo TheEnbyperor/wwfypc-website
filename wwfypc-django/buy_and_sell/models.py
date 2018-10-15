eb0b2e92fc7699471d150ee9bd672c2032f5acec
Q Misell <q@misell.cymru>
2018-10-15 18:26:46 +0100

    Buy and sell ordering

--- wwfypc-django/buy_and_sell/models.py
+++ wwfypc-django/buy_and_sell/models.py
@@ -1,19 +1,21 @@
 from django.db import models
+import main_site.models
 from main_site.models import COLOURS
 
 
-class ItemCategory(models.Model):
+class ItemCategory(main_site.models.OrderedModel):
     name = models.CharField(max_length=255, blank=False)
     colour = models.IntegerField(choices=COLOURS, blank=False, default=1)
 
     class Meta:
         verbose_name_plural = "Item categories"
+        ordering = ('order',)
 
     def __str__(self):
         return self.name
 
 
-class Item(models.Model):
+class Item(main_site.models.OrderedModel):
     category = models.ForeignKey(ItemCategory, related_name='items', on_delete=models.DO_NOTHING, blank=True)
     name = models.CharField(max_length=255, blank=False)
     price = models.DecimalField(decimal_places=2, max_digits=10, blank=False)
@@ -24,7 +26,7 @@ class Item(models.Model):
         return self.name
 
 
-class ItemImage(models.Model):
+class ItemImage(main_site.models.OrderedModel):
     item = models.ForeignKey(Item, related_name='images', on_delete=models.CASCADE)
     image = models.FileField(blank=False)
 
@@ -32,7 +34,7 @@ class ItemImage(models.Model):
         return str(self.id)
 
 
-class ItemSpec(models.Model):
+class ItemSpec(main_site.models.OrderedModel):
     item = models.ForeignKey(Item, related_name='specs', on_delete=models.CASCADE)
     name = models.CharField(max_length=255, blank=False)
     value = models.CharField(max_length=255, blank=False)
@@ -41,7 +43,7 @@ class ItemSpec(models.Model):
         return self.name
 
 
-class ItemPostage(models.Model):
+class ItemPostage(main_site.models.OrderedModel):
     item = models.ForeignKey(Item, related_name='postage', on_delete=models.CASCADE)
     name = models.CharField(max_length=255, blank=False)
     value = models.DecimalField(max_digits=10, decimal_places=2, blank=False)
