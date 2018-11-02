b7cfb2485d5784adf1a679a416edf6eb561dd000
Benjamin Misell <q@misell.cymru>
2018-11-02 10:50:10 +0000

    Scrolloverflow on build a pc

--- wwfypc-react/src/BuildPc/BuildPc.js
+++ wwfypc-react/src/BuildPc/BuildPc.js
@@ -230,6 +230,7 @@ export default class BuildPc extends Component {
             navigation: true,
             licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
             paddingBottom: "70px",
+            scrollOverflow: true,
         })
     }
 
