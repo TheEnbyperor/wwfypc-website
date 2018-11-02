e62e61fa4a5eb4c4408f91e360d91981045ccbcf
Benjamin Misell <q@misell.cymru>
2018-11-02 17:55:12 +0000

    Fix fullpage.js on service pages

--- wwfypc-react/src/Service/Service.js
+++ wwfypc-react/src/Service/Service.js
@@ -21,7 +21,7 @@ const SERVICES_QUERY = gql`
   }
 `;
 
-export default class Service extends Component {
+class InnerService extends Component {
     componentDidMount() {
         new window.fullpage(".Service", {
             navigationPosition: 'right',
@@ -35,26 +35,34 @@ export default class Service extends Component {
     }
 
     render() {
+        const data = this.props.data;
+
+        return <DocumentTitle title={data.servicePage.name + " | We Will Fix Your PC"}>
+            <div className="Service">
+                <div className="section" data-tooltip="Top" data-anchor="top"
+                     key={data.servicePage.sections.length}>
+                    <Top background={data.servicePage.headerBackground}/>
+                </div>
+                {data.servicePage.sections.map((section, i) => {
+                    return <div className="section" key={i} data-tooltip={section.title}
+                                data-anchor={section.id}>
+                        <Section data={section}/>
+                    </div>;
+                })}
+            </div>
+        </DocumentTitle>;
+    }
+}
+
+export default class Service extends Component {
+    render() {
         return (
             <Query query={SERVICES_QUERY} variables={{id: this.props.serviceId}}>
                 {({loading, error, data}) => {
                     if (loading) return null;
                     if (error) return <div className="section"><h1>Error</h1></div>;
 
-                    return <DocumentTitle title={data.servicePage.name + " | We Will Fix Your PC"}>
-                        <div className="Service">
-                            <div className="section" data-tooltip="Top" data-anchor="top"
-                                 key={data.servicePage.sections.length}>
-                                <Top background={data.servicePage.headerBackground}/>
-                            </div>
-                            {data.servicePage.sections.map((section, i) => {
-                                return <div className="section" key={i} data-tooltip={section.title}
-                                            data-anchor={section.id}>
-                                    <Section data={section}/>
-                                </div>;
-                            })}
-                        </div>
-                    </DocumentTitle>;
+                    return <InnerService data={data}/>;
                 }}
             </Query>
         );
