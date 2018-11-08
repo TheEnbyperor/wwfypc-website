199fa06ed086583fa16137824146d3d4114beb39
Q Misell <q@misell.cymru>
2018-11-08 19:20:44 +0000

    XVFB

--- wwfypc-django/start.sh
+++ wwfypc-django/start.sh
@@ -4,6 +4,6 @@ python3 manage.py collectstatic --no-input
 python3 manage.py migrate --settings wwfypc.settings_test
 echo Starting Gunicorn.
 export DJANGO_SETTINGS_MODULE=wwfypc.settings_test
-exec gunicorn wwfypc.wsgi:application \
+exec xvfb-run gunicorn wwfypc.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 5
