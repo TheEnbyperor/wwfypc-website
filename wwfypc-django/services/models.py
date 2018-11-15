9d6ccbe1a9d2dc58fa0628953d5e0a1f8093576c
Q Misell <q@misell.cymru>
2018-11-15 10:52:49 +0000

    Configurable text on service page top

--- wwfypc-django/services/models.py
+++ wwfypc-django/services/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 import main_site.models
 from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class ServicePage(main_site.models.OrderedModel):
     name = models.CharField(max_length=255, blank=False)
     url = models.CharField(max_length=255, blank=False)
     header_background = models.ImageField()
+    header_text = RichTextUploadingField(blank=False)
 
     def __str__(self):
         return self.name
