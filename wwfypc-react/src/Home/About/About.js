2af7044f296c96303380f0dc8a1cf921f406cdd1
Q Misell <q@misell.cymru>
2018-11-22 15:12:33 +0000

    Include missing gql

--- wwfypc-react/src/Home/About/About.js
+++ wwfypc-react/src/Home/About/About.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import "./style/About.scss";
 import {Query} from "react-apollo";
+import gql from "graphql-tag";
 import ReactHtmlParser from "react-html-parser";
 
 const ABOUT_QUERY = gql`
