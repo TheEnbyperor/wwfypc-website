d5b5f374f78661bbb537cc27de2b974a0c9f2077
Q Misell <q@misell.cymru>
2018-12-12 17:10:01 +0000

    Use rpl over sed
    Former-commit-id: eaf904188b533a07c7576dba4cd77fbe7b643aad

--- old-site/start.sh
+++ old-site/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-/bin/cat /etc/ssmtp/ssmtp.conf | /bin/sed "s/(pass)/$EMAIL_PASS/g" > /etc/ssmtp/ssmtp.conf
+/usr/bin/rpl "(pass)" $EMAIL_PASS /etc/ssmtp/ssmtp.conf
 /usr/sbin/apache2ctl -D FOREGROUND
